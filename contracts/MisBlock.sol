e28d350a16e428a510c85f747da9c732255494b4
Anderson Lee <cetbdb23@gmail.com>
2021-09-01 22:36:57 +0800

    modified erc20 token, implemented unit tests, and deploy script

--- contracts/MisBlock.sol
+++ contracts/MisBlock.sol
@@ -5,8 +5,7 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MisBlock is ERC20 {
-    constructor() public ERC20("MisBlock", "MIB") {
-
+    constructor() ERC20("MisBlock", "XBA") {
+        _mint(msg.sender, 1000000000000);
     }
-
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+const DEPLOYER_PRIVATE_KEY_RINKEBY = process.env.DEPLOYER_PRIVATE_KEY_RINKEBY || "";
+
 module.exports = {
-  solidity: "0.8.3",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      }
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY_RINKEBY}`],
+    },
+    kovan: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY_RINKEBY}`],
+    },
+  },
+  solidity: "0.8.2",
 };
