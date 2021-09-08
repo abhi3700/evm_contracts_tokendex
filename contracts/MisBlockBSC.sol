75bdc6250a974702e21cd07cd7236c443ed18673
Anderson Lee <cetbdb23@gmail.com>
2021-09-08 15:35:21 +0800

    implemented token taxes feature

--- contracts/MisBlockBSC.sol
+++ contracts/MisBlockBSC.sol
@@ -5,5 +5,7 @@ pragma solidity ^0.8.2;
 import "./MisBlockBase.sol";
 
 contract MisBlockBSC is MisBlockBase {
-    
+    constructor() MisBlockBase(0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F) {
+        
+    }
 }
--- contracts/MisBlockETH.sol
+++ contracts/MisBlockETH.sol
@@ -5,5 +5,7 @@ pragma solidity ^0.8.2;
 import "./MisBlockBase.sol";
 
 contract MisBlockETH is MisBlockBase {
-    
+    constructor() MisBlockBase(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D) {
+        
+    }
 }
